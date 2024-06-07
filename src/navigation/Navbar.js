c343b1c3f103877dabaf36171738a9b7c868ac5f
tomipoch <ft.fernandotomas@gmail.com>
2024-06-07 00:09:18 -0400

    Chatbot

--- src/navigation/Navbar.js
+++ src/navigation/Navbar.js
@@ -1,78 +1,96 @@
 import React from 'react';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Text } from 'react-native';
 
-//Screens
-import Inicio from "../screens/Inicio";
-import Busqueda from "../screens/Busqueda";
-import Camara from "../screens/Camara";
-import Perfil from "../screens/Perfil";
+// Screens
+import Inicio from '../screens/Inicio';
+import Busqueda from '../screens/Busqueda';
+import Camara from '../screens/Camara';
+import Perfil from '../screens/Perfil';
+import Asistencia from '../screens/Asistencia';
 
-//Iconos
+// Iconos
 import Feather from '@expo/vector-icons/Feather';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function InicioStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Inicio" component={Inicio} options={{ headerShown: true }} />
+      <Stack.Screen name="Asistencia" component={Asistencia} options={{ headerTitle: 'Asistencia' }} />
+    </Stack.Navigator>
+  );
+}
 
 function MyTabs() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen  
-                name="Inicio" 
-                component={Inicio} 
-                options={{
-                    tabBarLabel: ({ focused }) => (
-                        <Text style={{ color: focused ? 'green' : 'gray' }}>Inicio</Text>
-                    ),
-                    tabBarIcon: ({ focused }) => (
-                        <Feather name="home" size={24} color={focused ? 'green' : 'gray'} />
-                    ),
-                }}
-            />
-            <Tab.Screen  
-                name="Busqueda" 
-                component={Busqueda} 
-                options={{
-                    tabBarLabel: ({ focused }) => (
-                        <Text style={{ color: focused ? 'green' : 'gray' }}>Busqueda</Text>
-                    ),
-                    tabBarIcon: ({ focused }) => (
-                        <Feather name="search" size={24} color={focused ? 'green' : 'gray'} />
-                    ),
-                }}
-            />
-            <Tab.Screen  
-                name="Camara" 
-                component={Camara} 
-                options={{
-                    tabBarLabel: ({ focused }) => (
-                        <Text style={{ color: focused ? 'green' : 'gray' }}>Camara</Text>
-                    ),
-                    tabBarIcon: ({ focused }) => (
-                        <Feather name="camera" size={24} color={focused ? 'green' : 'gray'} />
-                    ),
-                }}
-            />
-            <Tab.Screen 
-                name="Perfil" 
-                component={Perfil} 
-                options={{
-                    tabBarLabel: ({ focused }) => (
-                        <Text style={{ color: focused ? 'green' : 'gray' }}>Perfil</Text>
-                    ),
-                    tabBarIcon: ({ focused }) => (
-                        <Feather name="user" size={24} color={focused ? 'green' : 'gray'} />
-                    ),
-                }}
-            />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: { fontSize: 12 },
+        tabBarStyle: { backgroundColor: 'white' },
+      }}
+    >
+      <Tab.Screen
+        name="Inicio"
+        component={InicioStack}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <Text style={{ color: focused ? 'green' : 'gray' }}>Inicio</Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <Feather name="home" size={24} color={focused ? 'green' : 'gray'} />
+          ),
+          headerShown: false, // Oculta el header de Inicio en la navegación de pestañas
+        }}
+      />
+      <Tab.Screen
+        name="Busqueda"
+        component={Busqueda}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <Text style={{ color: focused ? 'green' : 'gray' }}>Busqueda</Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <Feather name="search" size={24} color={focused ? 'green' : 'gray'} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Camara"
+        component={Camara}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <Text style={{ color: focused ? 'green' : 'gray' }}>Camara</Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <Feather name="camera" size={24} color={focused ? 'green' : 'gray'} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={Perfil}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <Text style={{ color: focused ? 'green' : 'gray' }}>Perfil</Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <Feather name="user" size={24} color={focused ? 'green' : 'gray'} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
 
 export default function Navigation() {
-    return (
-        <NavigationContainer>
-            <MyTabs />
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
 }
--- src/screens/Inicio.js
+++ src/screens/Inicio.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, ScrollView } from 'react-native';
 import { styled } from 'nativewind';
 import Feather from '@expo/vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 const StyledSafeAreaView = styled(SafeAreaView);
 const StyledView = styled(View);
@@ -19,40 +20,44 @@ const userData = {
   validity: '08/2024',
 };
 
-const criminals = ['Criminal 1', 'Criminal 2', 'Criminal 3']; // Agrega más criminales aquí si es necesario
+const criminals = ['Criminal 1', 'Criminal 2', 'Criminal 3'];
 
 const Inicio = () => {
+  const navigation = useNavigation();
+
   return (
     <StyledSafeAreaView className="flex-1 bg-white">
       <ScrollView>
         <StyledView className="p-4">
-          <StyledText className="text-green-600 text-2xl font-bold">Criminal Scan</StyledText>
-          <StyledText className="text-black text-3xl font-bold mt-2">Inicio</StyledText>
+          <StyledText className="text-black text-5xl font-bold mt-4">Inicio</StyledText>
 
           <StyledView className="rounded-2xl shadow-lg bg-white mt-4">
             <StyledView className="bg-green-600 rounded-t-2xl p-2">
               <StyledText className="text-white font-bold text-center">CREDENCIAL</StyledText>
             </StyledView>
             <StyledView className="flex-row p-4">
-              <StyledView className="w-16 h-16 bg-gray-400 rounded mr-4" />
+              <StyledView className="w-20 h-30 bg-gray-400 rounded mr-4" />
               <StyledView>
-                <StyledText className="text-black font-bold">{userData.name}</StyledText>
-                <StyledText className="text-gray-600">Rut: {userData.rut}</StyledText>
-                <StyledText className="text-gray-600">Grado: {userData.rank}</StyledText>
-                <StyledText className="text-gray-600">Código: {userData.code}</StyledText>
-                <StyledText className="text-gray-600">Escuadrón: {userData.squadron}</StyledText>
-                <StyledText className="text-gray-600">Grupo Sanguíneo: {userData.bloodGroup}</StyledText>
-                <StyledText className="text-gray-600">Validez: {userData.validity}</StyledText>
+                <StyledText className="text-black font-bold text-xl">{userData.name}</StyledText>
+                <StyledText className="text-gray-600 text-1xl">Rut: {userData.rut}</StyledText>
+                <StyledText className="text-gray-600 text-1xl">Grado: {userData.rank}</StyledText>
+                <StyledText className="text-gray-600 text-1xl">Código: {userData.code}</StyledText>
+                <StyledText className="text-gray-600 text-1xl">Escuadrón: {userData.squadron}</StyledText>
+                <StyledText className="text-gray-600 text-1xl">Grupo Sanguíneo: {userData.bloodGroup}</StyledText>
+                <StyledText className="text-gray-600 text-1xl">Validez: {userData.validity}</StyledText>
               </StyledView>
             </StyledView>
           </StyledView>
 
-          <StyledTouchableOpacity className="bg-green-600 rounded-2xl p-4 mt-6 w-full items-center flex-row justify-center">
+          <StyledTouchableOpacity
+            className="bg-green-600 rounded-2xl p-4 mt-6 w-full items-center flex-row justify-center"
+            onPress={() => navigation.navigate('Asistencia')}
+          >
             <Feather name="help-circle" size={24} color="white" className="mr-2" />
             <StyledText className="text-white font-bold">Asistencia</StyledText>
           </StyledTouchableOpacity>
 
-          <StyledText className="text-black text-xl font-bold mt-4">Criminales más Buscados</StyledText>
+          <StyledText className="text-black text-2xl font-bold mt-4">Criminales más Buscados</StyledText>
 
           <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} className="mt-4">
             {criminals.map((criminal, index) => (
